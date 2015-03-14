49a65242548e0fdef2d0bd4137b992db51d02f2b
APaxt <alex.paxton.email@gmail.com>
2015-03-13 23:15:42 -0400

    Update distributed_memory_system.c

---
+++ distributed_memory_system.c
@@ -0,0 +1,15 @@
+/*
+    File: distributed_memory_system.c
+    
+    Author: Alex Paxton
+    
+    Purpose: A Distributed Memory search algorithm which 
+    creates a matrix of the percentages of the presense 
+    of keywords and then uses this matrix to find the 
+    documents that most closely match the user's search 
+    queries by calculating the cosine of the angle between 
+    the vector of the search query and the vector of the 
+    documents.
+    
+    
+*/
